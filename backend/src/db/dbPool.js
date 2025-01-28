b9287746f6c889379a63928299d4050e6dc1fac4
Prabesh Sharma <prwshshrm@gmail.com>
2025-01-28 04:43:21 +0200

    Created postgres Dockerfile and implemented more secure workflow handling

--- backend/src/db/dbPool.js
+++ backend/src/db/dbPool.js
@@ -6,13 +6,11 @@ dotenv.config();
 const { Pool } = pkg;
 const isProduction = process.env.NODE_ENV === "production";
 
-const user = process.env.DB_USER;
-const password = process.env.DB_PASSWORD;
-const host = process.env.DB_HOST;
-const port = process.env.DB_PORT;
-const database = process.env.DB_NAME;
+const user = process.env.POSTGRES_USER;
+const password = process.env.POSTGRES_PASSWORD;
+const database = process.env.POSTGRES_DB;
 
-const connectionString = `postgresql://${user}:${password}@${host}:${port}/${database}`;
+const connectionString = `postgresql://${user}:${password}@localhost:5432/${database}`;
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DB_URL : connectionString,
